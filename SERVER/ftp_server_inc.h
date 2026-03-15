e65076dbd34169541efe21e8d38890be34acd2f1
wykim <ksq111@naver.com>
2026-03-15 07:05:09 -0700

    Add CLIENT and SERVER directories

---
+++ SERVER/ftp_server_inc.h
@@ -0,0 +1,25 @@
+#ifndef __FTP_SERVER_INC_H__
+#define __FTP_SERVER_INC_H__
+
+
+/* ===================================================
+ *	COMMON STANDARD INCLUDE HEADER
+ * ===================================================
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+
+
+
+
+#endif
